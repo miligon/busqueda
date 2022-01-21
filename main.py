33f3c8980aa3eac1cdedbc6ef2a34b2c8d55455e
miligon <miguel.limon@upaep.edu.mx>
2022-01-21 16:06:26 -0600

    Graficador

--- main.py
+++ main.py
@@ -196,6 +196,29 @@ mapa2 = {"Arad":["Sibiu","Timisoara","Zerind"],
 "Eforie":["Hirsova"]
 }
 
+mapa3={
+"Arad":{"nodes":["Zerind","Sibiu","Timisoara"],"coord":[67,133]},
+"Zerind":{"nodes":["Oradea","Arad"],"coord":[92,80]},
+"Oradea":{"nodes":["Zerind","Sibiu"],"coord":[122,25]},
+"Timisoara":{"nodes":["Lugoj","Arad"],"coord":[72,245]},
+"Lugoj":{"nodes":["Timisoara","Mehadia"],"coord":[168,287]},
+"Dobreta":{"nodes":["Mehadia","Craiova"],"coord":[169,395]},
+"Mehadia":{"nodes":["Lugoj","Dobreta"],"coord":[173,340]},
+"Sibiu":{"nodes":["Arad","Oradea","Rimnicu Vilcea","Fagaras"],"coord":[227,181]},
+"Fagaras":{"nodes":["Sibiu","Bucharest"],"coord":[362,191]},
+"Rimnicu Vilcea":{"nodes":["Sibiu","Pitesti","Craiova"],"coord":[261,244]},
+"Craiova":{"nodes":["Pitesti","Rimnicu Vilcea","Dobreta"],"coord":[287,410]},
+"Pitesti":{"nodes":["Rimnicu Vilcea","Craiova","Bucharest"],"coord":[381,302]},
+"Bucharest":{"nodes":["Fagaras","Pitesti","Giurgiu","Urziceni"],"coord":[488,356]},
+"Urziceni":{"nodes":["Bucharest","Hirsova","Vaslui"],"coord":[565,325]},
+"Vaslui":{"nodes":["Urziceni","Iasi"],"coord":[636,197]},
+"Iasi":{"nodes":["Neamt","Vaslui"],"coord":[587,114]},
+"Neamt":{"nodes":["Iasi"],"coord":[495,71]},
+"Hirsova":{"nodes":["Urziceni","Eforie"],"coord":[669,326]},
+"Eforie":{"nodes":["Hirsova"],"coord":[707,402]},
+"Giurgiu":{"nodes":["Bucharest"],"coord":[453,433]}
+}
+
 agent = Agente(mapa2)
 agent.setInicio("Arad")
 agent.setFinal("Eforie")
